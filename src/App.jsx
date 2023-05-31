76a51cf9f168293d4915d42f7b30f8a72efa4ce5
mrzahidxy <mrzahidxy@gmail.com>
2023-05-31 08:36:35 +0600

    fix: router issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Messenger from "./pages/messenger/Messenger.jsx";
+import Login from "./pages/login/Login.jsx";
 
 function App() {
-  return <div className="text-red-500">HELLO Y</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/messenger" element={<Messenger />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Messenger from "./pages/messenger/Messenger.jsx";
-import Login from "./pages/login/Login.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/messenger",
-    element: <Messenger />,
-  },
-  {
-    path: "/login",
-    element: <Login/>,
-  },
-]);
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+<App/>
   </React.StrictMode>
 );
