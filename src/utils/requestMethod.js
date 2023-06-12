0bba4439812ef562c3a8e9077349cb8fb22ec509
mrzahidxy <mrzahidxy@gmail.com>
2023-06-13 00:33:02 +0600

    url change

--- src/utils/requestMethod.js
+++ src/utils/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "http://social-media-api-zahid.vercel.app:8080/api";
 const user = JSON.parse(localStorage.getItem("currentUser"));
 const TOKEN = user?.accessToken;
 
